b938ae7bbe180b0d9d8001538c896f9abbb704e5
Alexander Lazarenko <ferrosful@gmail.com>
2022-08-17 22:07:02 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const typed = new Typed(
+    ".typing", {
+        strings: ["Freelancer", ".NET Web Developer", "Big Data Engineer"],
+        typeSpeed: 100,
+        BackSpeed: 80,
+        loop: true
+});
+
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavListLength = navList.length;
+
+for (let i = 0; i < totalNavListLength; i++) {
+        const anchorElement = navList[i].querySelector('a');
+        anchorElement.addEventListener('click', function() {
+                for (let j = 0; j < totalNavListLength; j++) {
+                        navList[j].querySelector('a')
+                            .classList.remove('active');
+                }
+                this.classList.add('active');
+        })
+}
+
+function sendMail() {
+        const link = "mailto:ferrosful@gmail.com"
+            + "?cc=valentinnestovich@gmail.com"
+            + "&subject=" + encodeURIComponent(
+                document.getElementById('subject-input').value
+                + " - From " + document.getElementById('name-input').value
+                + " !" + document.getElementById('reason-input').value.toUpperCase()
+            )
+            + "&body=" + encodeURIComponent(document.getElementById('message-text-input').value)
+        ;
+        window.location.href = link;
+}
+
---
+++ style-switcher.js
@@ -0,0 +1,45 @@
+const styleSwitcherToggle =
+    document.querySelector('.style-switcher-toggler');
+const styleSwitcherElement =
+    document.querySelector('.style-switcher');
+
+styleSwitcherToggle.addEventListener('click', () => {
+        styleSwitcherElement.classList.toggle('open');
+});
+
+addEventListener('scroll', () => {
+   if (styleSwitcherElement.classList.contains('open'))
+        styleSwitcherElement.classList.remove('open');
+});
+
+
+const alternateStyles =
+    document.querySelectorAll('.alternate-style');
+
+function setActiveStyle(color) {
+    alternateStyles.forEach(style => {
+        if (color === style.getAttribute('title')) {
+            style.removeAttribute('disabled');
+        } else {
+            style.setAttribute('disabled', 'true');
+        }
+    });
+}
+
+
+const dayNightElement =
+    document.querySelector('.day-night');
+
+dayNightElement.addEventListener('click', () => {
+    dayNightElement.querySelector('i').classList.toggle('fa-sun');
+    dayNightElement.querySelector('i').classList.toggle('fa-moon');
+    document.body.classList.toggle('dark');
+});
+
+window.addEventListener('load', () => {
+    if (document.body.classList.contains('dark')) {
+        dayNightElement.querySelector('i').classList.add('fa-sun');
+    } else {
+        dayNightElement.querySelector('i').classList.add('fa-moon');
+    }
+});
