3f0016edecafd57130f59f8750b353117aefb798
Alexander Lazarenko <ferrosful@gmail.com>
2022-08-26 22:46:37 +0300

    Added some minor improvements to website look and filled in the information about myself.

--- scripts/script.js
+++ scripts/script.js
@@ -4,41 +4,43 @@ const typed = new Typed(
         typeSpeed: 100,
         BackSpeed: 80,
         loop: true
-});
+    });
 
 const nav = document.querySelector('.nav'),
     navList = nav.querySelectorAll('li'),
     totalNavListLength = navList.length;
 
 for (let i = 0; i < totalNavListLength; i++) {
-        const anchorElement = navList[i].querySelector('a');
-        anchorElement.addEventListener('click', () => {
-                for (let j = 0; j < totalNavListLength; j++) {
-                        navList[j].querySelector('a')
-                            .classList.remove('active');
-                }
-                this.classList.add('active');
-        })
+    const anchorElement = navList[i].querySelector('a');
+    anchorElement.addEventListener('click', () => {
+        for (let j = 0; j < totalNavListLength; j++) {
+            navList[j].querySelector('a')
+                .classList.remove('active');
+        }
+        anchorElement.classList.add('active');
+    })
 }
 
 function sendMail() {
-        const link = "mailto:ferrosful@gmail.com"
-            + "?cc=valentinnestovich@gmail.com"
-            + "&subject=" + encodeURIComponent(
-                document.getElementById('subject-input').value
-                + " - From " + document.getElementById('name-input').value
-                + " !" + document.getElementById('reason-input').value.toUpperCase()
-            )
-            + "&body=" + encodeURIComponent(document.getElementById('message-text-input').value)
-        ;
-        window.location.href = link;
+    const link = "mailto:ferrosful@gmail.com"
+        + "?cc=valentinnestovich@gmail.com"
+        + "&subject=" + encodeURIComponent(
+            document.getElementById('subject-input').value
+            + " - From " + document.getElementById('name-input').value
+            + " !" + document.getElementById('reason-input').value.toUpperCase()
+        )
+        + "&body=" + encodeURIComponent(document.getElementById('message-text-input').value)
+    ;
+    window.location.href = link;
 }
 
-window.addEventListener('scroll', (e) => {
-        const scrollDownEl = document.querySelector('.scroll-down');
-        if (document.querySelector('.nav a').classList.contains('active')) {
-                scrollDownEl.classList.remove('hidden');
-        } else {
-                scrollDownEl.classList.add('hidden');
+$(window).scroll(() => {
+    const scrollTop = $(this).scrollTop();
+
+    $('.scroll-down').css({
+        opacity: function () {
+            const elementHeight = $(this).height() * 10;
+            return ((elementHeight - scrollTop) / elementHeight);
         }
-})
+    });
+});
