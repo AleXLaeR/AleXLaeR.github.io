e45fda93a9e9747a7df0084a4d1e50502972be5c
Alexander Lazarenko <ferrosful@gmail.com>
2022-09-05 13:21:31 +0300

    Added email sending feature using "emailJS" alongside minor UX improvements.

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,8 @@ const typed = new Typed(
         typeSpeed: 100,
         BackSpeed: 80,
         loop: true
-    });
+    }
+);
 
 const nav = document.querySelector('.nav'),
     navList = nav.querySelectorAll('li'),
@@ -21,19 +22,6 @@ for (let i = 0; i < totalNavListLength; i++) {
     })
 }
 
-function sendMail() {
-    const link = "mailto:ferrosful@gmail.com"
-        + "?cc=valentinnestovich@gmail.com"
-        + "&subject=" + encodeURIComponent(
-            document.getElementById('subject-input').value
-            + " - From " + document.getElementById('name-input').value
-            + " !" + document.getElementById('reason-input').value.toUpperCase()
-        )
-        + "&body=" + encodeURIComponent(document.getElementById('message-text-input').value)
-    ;
-    window.location.href = link;
-}
-
 $(window).scroll(() => {
     const scrollTop = $(this).scrollTop();
 
@@ -44,3 +32,47 @@ $(window).scroll(() => {
         }
     });
 });
+
+const sendEmailBtnEl = document.getElementById('send-email-btn');
+
+sendEmailBtnEl.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const name = document.getElementById('name-input').value;
+    const email = document.getElementById('email-input').value;
+    const subject = document.getElementById('subject-input').value;
+    const message = document.getElementById('message-text-input').value;
+
+    if (name && email && subject && message) {
+        sendEmail(name, email, subject, message);
+        onSuccessfulInput();
+    } else {
+        onEmptyInputField();
+    }
+});
+
+function onEmptyInputField() {
+    swal({
+        title: 'Error!',
+        text: 'Input field cannot be empty!',
+        icon: 'error',
+        button: 'Try Again',
+    })
+}
+
+function onSuccessfulInput() {
+    swal({
+        title: 'Email sent!',
+        text: 'I will reply as soon as possible!',
+        icon: 'success',
+    })
+}
+
+function sendEmail(name, email, subject, message) {
+    emailjs.send('service_jcfclw8', 'template_31tkh2l', {
+        from_name: email,
+        to_name: name,
+        subject: subject,
+        message: message,
+    });
+}
